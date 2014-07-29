704aeaae7c600acf4ce1d0384d564239aafcb020
David Walker <testmad@thinstack.net>
2014-07-29 14:31:50 -0400

    Events: Changed fog density in lava.

--- src/main/java/gaiamod/handlers/GaiaModEventHandler.java
+++ src/main/java/gaiamod/handlers/GaiaModEventHandler.java
@@ -4,6 +4,8 @@ import gaiamod.armor.ModArmor;
 
 import java.util.UUID;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialLiquid;
 import net.minecraft.client.Minecraft;
@@ -27,8 +29,6 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class GaiaModEventHandler {
 	
-	private static final UUID wtvID = UUID.fromString("641470a0-ff51-4598-bdae-210184bbe083");
-
 	@SubscribeEvent
 	public void onFOVUpdate(FOVUpdateEvent event){
 		event.setResult(Result.DEFAULT);
@@ -36,30 +36,80 @@ public class GaiaModEventHandler {
 
 	@SubscribeEvent
 	public void EntityViewRenderEvent(EntityViewRenderEvent.FogDensity event){
+		
 		EntityLivingBase entityLiving = event.entity;
 		
-//		if(entityLiving instanceof EntityPlayer){
+		if(entityLiving instanceof EntityPlayer && entityLiving.worldObj.isRemote){
 			EntityPlayer entityPlayer = (EntityPlayer) entityLiving;
 			
 			ItemStack helm = entityPlayer.getCurrentArmor(3);
 			ItemStack chest = entityPlayer.getCurrentArmor(2);
 			ItemStack legs = entityPlayer.getCurrentArmor(1);
 			ItemStack boots = entityPlayer.getCurrentArmor(0);
-			
-			if(helm !=null && chest !=null & legs !=null && boots !=null){
-				if (helm.getItem() == ModArmor.fireHelmet && chest.getItem() == ModArmor.fireChest && legs.getItem() == ModArmor.fireLeggings && boots.getItem() == ModArmor.fireBoots){						if(entityPlayer.isInsideOfMaterial(Material.lava)){
-
+		
+			if(helm !=null && chest !=null & legs !=null && boots !=null && !entityPlayer.capabilities.isCreativeMode){
+				if (helm.getItem() == ModArmor.fireHelmet && chest.getItem() == ModArmor.fireChest && legs.getItem() == ModArmor.fireLeggings && boots.getItem() == ModArmor.fireBoots && !entityPlayer.capabilities.isCreativeMode){
+					if(event.entity.isInsideOfMaterial(Material.lava)){
 						event.setCanceled(true);
-						event.density = 0.5f;
-						
-					}else{
-						//event.setCanceled(false);
-						//event.renderer.updateRenderer();
-						
+			            event.density = 0.2f;
+			            GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_EXP);
+					}
+				}
+				if (helm.getItem() == ModArmor.chaosHelmet && chest.getItem() == ModArmor.chaosChest && legs.getItem() == ModArmor.chaosLeggings && boots.getItem() == ModArmor.chaosBoots && !entityPlayer.capabilities.isCreativeMode){
+					if(event.entity.isInsideOfMaterial(Material.lava)){
+						event.setCanceled(true);
+			            event.density = 0.2f;
+			            GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_EXP);
+					}
+				}
+				if (helm.getItem() == ModArmor.orderHelmet && chest.getItem() == ModArmor.orderChest && legs.getItem() == ModArmor.orderLeggings && boots.getItem() == ModArmor.orderBoots && !entityPlayer.capabilities.isCreativeMode){
+					if(event.entity.isInsideOfMaterial(Material.lava)){
+						event.setCanceled(true);
+			            event.density = 0.2f;
+			            GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_EXP);
 					}
 				}
 			}
-//		}		
+		}
+	}
+	
+	@SubscribeEvent
+	public void EntityViewRenderEvent(EntityViewRenderEvent.FogColors event){
+		
+		EntityLivingBase entityLiving = event.entity;
+		
+		if(entityLiving instanceof EntityPlayer && entityLiving.worldObj.isRemote){
+			EntityPlayer entityPlayer = (EntityPlayer) entityLiving;
+			
+			ItemStack helm = entityPlayer.getCurrentArmor(3);
+			ItemStack chest = entityPlayer.getCurrentArmor(2);
+			ItemStack legs = entityPlayer.getCurrentArmor(1);
+			ItemStack boots = entityPlayer.getCurrentArmor(0);
+		
+			if(helm !=null && chest !=null & legs !=null && boots !=null && !entityPlayer.capabilities.isCreativeMode){
+				if (helm.getItem() == ModArmor.fireHelmet && chest.getItem() == ModArmor.fireChest && legs.getItem() == ModArmor.fireLeggings && boots.getItem() == ModArmor.fireBoots && !entityPlayer.capabilities.isCreativeMode){
+					if(event.entity.isInsideOfMaterial(Material.lava)){
+			            event.red = 0.5f;
+			            event.green = 0;
+			            event.blue = 0;
+					}
+				}
+				if (helm.getItem() == ModArmor.chaosHelmet && chest.getItem() == ModArmor.chaosChest && legs.getItem() == ModArmor.chaosLeggings && boots.getItem() == ModArmor.chaosBoots && !entityPlayer.capabilities.isCreativeMode){
+					if(event.entity.isInsideOfMaterial(Material.lava)){
+			            event.red = 0.5f;
+			            event.green = 0;
+			            event.blue = 0;
+					}
+				}
+				if (helm.getItem() == ModArmor.orderHelmet && chest.getItem() == ModArmor.orderChest && legs.getItem() == ModArmor.orderLeggings && boots.getItem() == ModArmor.orderBoots && !entityPlayer.capabilities.isCreativeMode){
+					if(event.entity.isInsideOfMaterial(Material.lava)){
+			            event.red = 0.5f;
+			            event.green = 0;
+			            event.blue = 0;
+					}
+				}
+			}
+		}
 	}
 	
 	@SubscribeEvent
@@ -73,49 +123,35 @@ public class GaiaModEventHandler {
 			ItemStack chest = entityPlayer.getCurrentArmor(2);
 			ItemStack legs = entityPlayer.getCurrentArmor(1);
 			ItemStack boots = entityPlayer.getCurrentArmor(0);
-			
 
-			
-			
 			if(helm !=null && chest !=null & legs !=null && boots !=null && !entityPlayer.capabilities.isCreativeMode){
 				if (helm.getItem() == ModArmor.earthHelmet && chest.getItem() == ModArmor.earthChest && legs.getItem() == ModArmor.earthLeggings && boots.getItem() == ModArmor.earthBoots && !entityPlayer.capabilities.isCreativeMode){
-					
 					if(!entityPlayer.isSneaking()){
 						entityPlayer.stepHeight = 1.0f;
 					}else{
 					entityPlayer.stepHeight = 0.5f;
 					} 
-					
 				}
-				
 				if (helm.getItem() == ModArmor.chaosHelmet && chest.getItem() == ModArmor.chaosChest && legs.getItem() == ModArmor.chaosLeggings && boots.getItem() == ModArmor.chaosBoots && !entityPlayer.capabilities.isCreativeMode){
-					
 					if(!entityPlayer.isSneaking()){
 						entityPlayer.stepHeight = 1.0f;
 					}else{
 					entityPlayer.stepHeight = 0.5f;
 					} 
-					
 				}
-				
 				if (helm.getItem() == ModArmor.orderHelmet && chest.getItem() == ModArmor.orderChest && legs.getItem() == ModArmor.orderLeggings && boots.getItem() == ModArmor.orderBoots && !entityPlayer.capabilities.isCreativeMode){
-					
 					if(!entityPlayer.isSneaking()){
 						entityPlayer.stepHeight = 1.0f;
 					}else{
 					entityPlayer.stepHeight = 0.5f;
 					} 
-					
 				}
 			}else{
 				entityPlayer.stepHeight = 0.5f;
-				
 			}
 		}
-		
 		if(entityLiving instanceof EntityPlayer){
 			EntityPlayer entityPlayer = (EntityPlayer) entityLiving;
-			
 			
 			ItemStack helm = entityPlayer.getCurrentArmor(3);
 			ItemStack chest = entityPlayer.getCurrentArmor(2);
@@ -124,20 +160,15 @@ public class GaiaModEventHandler {
 			
 			if(helm !=null && chest !=null & legs !=null && boots !=null){
 				if (helm.getItem() == ModArmor.earthHelmet && chest.getItem() == ModArmor.earthChest && legs.getItem() == ModArmor.earthLeggings && boots.getItem() == ModArmor.earthBoots && !entityPlayer.capabilities.isCreativeMode){
-					
 					entityPlayer.capabilities.setPlayerWalkSpeed(0.2f);
 					entityPlayer.sendPlayerAbilities();
 				}
-				
 				if (helm.getItem() == ModArmor.fireHelmet && chest.getItem() == ModArmor.fireChest && legs.getItem() == ModArmor.fireLeggings && boots.getItem() == ModArmor.fireBoots && !entityPlayer.capabilities.isCreativeMode){
-					if(entityPlayer.isInsideOfMaterial(Material.lava) || entityPlayer.isInsideOfMaterial(MaterialLiquid.lava)){
+					if(entityPlayer.isInsideOfMaterial(Material.lava)){
 						entityPlayer.capabilities.allowFlying = true;
 						entityPlayer.capabilities.isFlying = true;
 						entityPlayer.capabilities.setFlySpeed(.02f);
 						entityPlayer.sendPlayerAbilities();
-						
-						
-
 					}else{
 						entityPlayer.capabilities.allowFlying = false;
 						entityPlayer.capabilities.isFlying = false;
@@ -145,17 +176,13 @@ public class GaiaModEventHandler {
 						entityPlayer.sendPlayerAbilities();
 					}
 				}
-				
 				if (helm.getItem() == ModArmor.windHelmet && chest.getItem() == ModArmor.windChest && legs.getItem() == ModArmor.windLeggings && boots.getItem() == ModArmor.windBoots && !entityPlayer.capabilities.isCreativeMode){
-					
 					entityPlayer.fallDistance = 0.0f;
 					entityPlayer.jumpMovementFactor = .05F;
 				}
-				
 				if (helm.getItem() == ModArmor.waterHelmet && chest.getItem() == ModArmor.waterChest && legs.getItem() == ModArmor.waterLeggings && boots.getItem() == ModArmor.waterBoots && !entityPlayer.capabilities.isCreativeMode){
 					entityPlayer.setAir(300);
 					if(entityPlayer.isInWater()){
-						
 						entityPlayer.capabilities.allowFlying = true;
 						entityPlayer.capabilities.isFlying = true;
 						entityPlayer.capabilities.setFlySpeed(.03f);
@@ -167,19 +194,12 @@ public class GaiaModEventHandler {
 						entityPlayer.sendPlayerAbilities();
 					}
 				}
-				
 				if (helm.getItem() == ModArmor.heartHelmet && chest.getItem() == ModArmor.heartChest && legs.getItem() == ModArmor.heartLeggings && boots.getItem() == ModArmor.heartBoots && !entityPlayer.capabilities.isCreativeMode){
-					
 					entityPlayer.getFoodStats().addStats(20, 5.0f);
 				}
-				
 				if (helm.getItem() == ModArmor.stormHelmet && chest.getItem() == ModArmor.stormChest && legs.getItem() == ModArmor.stormLeggings && boots.getItem() == ModArmor.stormBoots && !entityPlayer.capabilities.isCreativeMode){
-
-					
 				}
-				
 				if (helm.getItem() == ModArmor.chaosHelmet && chest.getItem() == ModArmor.chaosChest && legs.getItem() == ModArmor.chaosLeggings && boots.getItem() == ModArmor.chaosBoots && !entityPlayer.capabilities.isCreativeMode){
-					
 					entityPlayer.getFoodStats().addStats(20, 5.0f);
 					entityPlayer.setAir(300);
 					entityPlayer.fallDistance = 0.0f;
@@ -187,11 +207,8 @@ public class GaiaModEventHandler {
 					entityPlayer.capabilities.setPlayerWalkSpeed(0.2f);
 					entityPlayer.capabilities.allowFlying = true;
 					entityPlayer.sendPlayerAbilities();
-					
 				}
-				
 				if (helm.getItem() == ModArmor.orderHelmet && chest.getItem() == ModArmor.orderChest && legs.getItem() == ModArmor.orderLeggings && boots.getItem() == ModArmor.orderBoots && !entityPlayer.capabilities.isCreativeMode){
-
 					entityPlayer.getFoodStats().addStats(20, 5.0f);
 					entityPlayer.setAir(300);
 					entityPlayer.fallDistance = 0.0f;
@@ -200,14 +217,12 @@ public class GaiaModEventHandler {
 					entityPlayer.capabilities.allowFlying = true;
 					entityPlayer.sendPlayerAbilities();
 				}
-				
 			}else{
 				entityPlayer.capabilities.setPlayerWalkSpeed(0.1f);
 				entityPlayer.capabilities.setFlySpeed(.05f);
 				entityPlayer.capabilities.allowFlying = false;
 				entityPlayer.capabilities.isFlying = false;
 				entityPlayer.sendPlayerAbilities();
-				
 			}
 		}
 	}
@@ -224,28 +239,22 @@ public class GaiaModEventHandler {
 			ItemStack boots = entityPlayer.getCurrentArmor(0);
 			
 			if(helm !=null && chest !=null & legs !=null && boots !=null){
-				
 				if (helm.getItem() == ModArmor.fireHelmet && chest.getItem() == ModArmor.fireChest && legs.getItem() == ModArmor.fireLeggings && boots.getItem() == ModArmor.fireBoots && !entityPlayer.capabilities.isCreativeMode){
 					if(event.source != null && event.source == DamageSource.lava || event.source == DamageSource.inFire || event.source == DamageSource.onFire){
 						event.setCanceled(true);
 						entityPlayer.extinguish();
-
 					}
 				}
-				
 				if (helm.getItem() == ModArmor.chaosHelmet && chest.getItem() == ModArmor.chaosChest && legs.getItem() == ModArmor.chaosLeggings && boots.getItem() == ModArmor.chaosBoots && !entityPlayer.capabilities.isCreativeMode){
 					if(event.source != null && event.source == DamageSource.lava || event.source == DamageSource.inFire || event.source == DamageSource.onFire){
 						event.setCanceled(true);
 						entityPlayer.extinguish();
-
 					}
 				}
-				
 				if (helm.getItem() == ModArmor.orderHelmet && chest.getItem() == ModArmor.orderChest && legs.getItem() == ModArmor.orderLeggings && boots.getItem() == ModArmor.orderBoots && !entityPlayer.capabilities.isCreativeMode){
 					if(event.source != null && event.source == DamageSource.lava || event.source == DamageSource.inFire || event.source == DamageSource.onFire){
 						event.setCanceled(true);
 						entityPlayer.extinguish();
-
 					}
 				}
 			}
@@ -259,15 +268,25 @@ public class GaiaModEventHandler {
 		
 		if(attackerEntity.getEntity() instanceof EntityPlayer){
 			EntityPlayer entityPlayer = (EntityPlayer) attackerEntity.getEntity();
+			
 			ItemStack itemHeld = entityPlayer.getHeldItem();
-			if(itemHeld.getItem() == Items.bow){
-				double i = attackedEntity.posX;
-				double j = attackedEntity.posY;
-				double k = attackedEntity.posZ;
+			ItemStack helm = entityPlayer.getCurrentArmor(3);
+			ItemStack chest = entityPlayer.getCurrentArmor(2);
+			ItemStack legs = entityPlayer.getCurrentArmor(1);
+			ItemStack boots = entityPlayer.getCurrentArmor(0);
+			
+			if(helm !=null && chest !=null & legs !=null && boots !=null){
+				if (helm.getItem() == ModArmor.stormHelmet && chest.getItem() == ModArmor.stormChest && legs.getItem() == ModArmor.stormLeggings && boots.getItem() == ModArmor.stormBoots && !entityPlayer.capabilities.isCreativeMode){
+					if(itemHeld.getItem() == Items.bow){
+						double i = attackedEntity.posX;
+						double j = attackedEntity.posY;
+						double k = attackedEntity.posZ;
 				
-				EntityLightningBolt entityLightningBolt = new EntityLightningBolt(entityPlayer.worldObj, i, j, k);
+						EntityLightningBolt entityLightningBolt = new EntityLightningBolt(entityPlayer.worldObj, i, j, k);
 				
-				entityPlayer.worldObj.addWeatherEffect(entityLightningBolt);
+						entityPlayer.worldObj.addWeatherEffect(entityLightningBolt);
+					}
+				}
 			}
 		}
 	}
@@ -287,19 +306,14 @@ public class GaiaModEventHandler {
 			
 			if(helm !=null && chest !=null & legs !=null && boots !=null){
 				if (helm.getItem() == ModArmor.windHelmet && chest.getItem() == ModArmor.windChest && legs.getItem() == ModArmor.windLeggings && boots.getItem() == ModArmor.windBoots && !entityPlayer.capabilities.isCreativeMode){
-
 					entityPlayer.motionY +=addY;
 					entityPlayer.velocityChanged = true;
 				}
-				
 				if (helm.getItem() == ModArmor.chaosHelmet && chest.getItem() == ModArmor.chaosChest && legs.getItem() == ModArmor.chaosLeggings && boots.getItem() == ModArmor.chaosBoots && !entityPlayer.capabilities.isCreativeMode){
-
 					entityPlayer.motionY +=addY;
 					entityPlayer.velocityChanged = true;
 				}
-				
 				if (helm.getItem() == ModArmor.orderHelmet && chest.getItem() == ModArmor.orderChest && legs.getItem() == ModArmor.orderLeggings && boots.getItem() == ModArmor.orderBoots && !entityPlayer.capabilities.isCreativeMode){
-
 					entityPlayer.motionY +=addY;
 					entityPlayer.velocityChanged = true;
 				}
